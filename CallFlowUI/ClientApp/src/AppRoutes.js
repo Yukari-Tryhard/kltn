32649707e1f0e255549c0f4095955625c40ba022
tinnt <tinnguyen2682k1@gmail.com>
2023-08-21 00:05:02 +0700

    Add NotFound Page

--- CallFlowUI/ClientApp/src/AppRoutes.js
+++ CallFlowUI/ClientApp/src/AppRoutes.js
@@ -2,6 +2,7 @@ import { useRoutes, Navigate as Redirect} from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignIn from "./pages/signin/SignIn";
 import SignUp from "./pages/signup/SignUp";
+import NotFoundPage from "./pages/notfound/NotFoundPage";
 import ProtectedRoute from "./common/auth/ProtectedRoute";
 
 const AppRoutes = () => {
@@ -22,6 +23,10 @@ const AppRoutes = () => {
   },
   {
     path: '/*',
+    element: <NotFoundPage/>,
+  },
+  {
+    path: '/app',
     element: <ProtectedRoute/>,
     children: [
       {
--- CallFlowUI/ClientApp/src/pages/notfound/NotFoundPage.js
+++ CallFlowUI/ClientApp/src/pages/notfound/NotFoundPage.js
@@ -1,49 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button } from '@material-ui/core';
-import { ErrorOutline } from '@material-ui/icons';
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-  },
-  icon: {
-    fontSize: '6rem',
-    color: theme.palette.error.main,
-  },
-  heading: {
-    margin: theme.spacing(2),
-    fontWeight: 'bold',
-  },
-  message: {
-    marginBottom: theme.spacing(2),
-    textAlign: 'center',
-  },
-  link: {
-    textDecoration: 'none',
-  },
-}));
+import React from "react";
+import { Link } from "react-router-dom";
+import { Badge } from "primereact/badge";
+import { InputText } from "primereact/inputtext";
 
 const NotFoundPage = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.container}>
-      <ErrorOutline className={classes.icon} />
-      <Typography variant="h4" className={classes.heading}>
-        Page Not Found
-      </Typography>
-      <Typography variant="body1" className={classes.message}>
-        The requested page could not be found.
-      </Typography>
-      <Button component={Link} to="/home/usage" variant="contained" color="primary">
-        Go to Home
-      </Button>
+    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <Badge value="404 error"></Badge>
+      <div className="text-5xl font-bold mt-4">We've lost this page</div>
+      <div className="text-lg mt-2">
+        Sorry, the page you are looking for doesn't exist or has been moved.
+      </div>
+      <span className="p-input-icon-left mt-10">
+        <i className="pi pi-search" />
+        <InputText placeholder="Search our site" />
+      </span>
     </div>
   );
 };
