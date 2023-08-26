0971bf5c76e0b9e7a7335f7566899fdb2c09aa87
minhducnt <minhduc.nt1602@gmail.com>
2023-08-26 20:41:44 +0700

    Add gitignore

--- CallFlowUI/ClientApp/src/common/components/SideNav.js
+++ CallFlowUI/ClientApp/src/common/components/SideNav.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SideNav = () => {
-  const [activeNavItem, setActiveNavItem] = useState('');
+    const [activeNavItem, setActiveNavItem] = useState('');
 
-//   const handleNavItemClick = (navItem) => {
-//     setActiveNavItem(navItem);
-//   };
+    //   const handleNavItemClick = (navItem) => {
+    //     setActiveNavItem(navItem);
+    //   };
 
-  return (
-    <div></div>
-  );
+    return <div></div>;
 };
-
-// export default SideNav;
+export default SideNav;
