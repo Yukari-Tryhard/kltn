cad74de54178a081df7abddb80e7fc38d70435d7
minhducnt <minhduc.nt1602@gmail.com>
2023-10-17 23:57:40 +0700

    Add Notification

--- AdminUI/ClientApp/src/containers/Layout.js
+++ AdminUI/ClientApp/src/containers/Layout.js
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
+import { removeNotificationMessage } from '../store/common/HeaderSlice';
+
 import ModalLayout from './ModalLayout';
 import PageContent from './PageContent';
 import LeftSidebar from './LeftSidebar';
 import RightSidebar from './RightSidebar';
 
 function Layout() {
+	const dispatch = useDispatch();
+	const { newNotificationMessage, newNotificationStatus } = useSelector((state) => state.header);
+
+	useEffect(() => {
+		if (newNotificationMessage !== '') {
+			if (newNotificationStatus === 1) NotificationManager.success(newNotificationMessage, 'Success');
+			if (newNotificationStatus === 0) NotificationManager.error(newNotificationMessage, 'Error');
+			dispatch(removeNotificationMessage());
+		}
+	}, [dispatch, newNotificationMessage, newNotificationStatus]);
+
 	return (
 		<>
 			{/* Left drawer - containing page content and side bar (always open) */}
@@ -15,6 +33,9 @@ function Layout() {
 
 			{/* Right drawer - containing secondary content like notifications list etc.. */}
 			<RightSidebar />
+
+			{/** Notification layout container */}
+			<NotificationContainer />
 
 			{/* Modal layout container */}
 			<ModalLayout />
